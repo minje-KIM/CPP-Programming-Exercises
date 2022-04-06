47a6b7e18f2c50344782874b46e4b057a9c897c3
minje-KIM <61033374+minje-KIM@users.noreply.github.com>
2022-04-06 18:05:51 +0900

    Week4_Homework

---
+++ Week4_Homework/basic_assignment_1/puzzle_15/puzzle_15/main.cpp
@@ -0,0 +1,12 @@
+#include "Ranking.h"
+extern int PlayFifteenPuzzle();
+
+int main()
+{
+	LoadRanking("ranking.txt");
+	int rank = PlayFifteenPuzzle();
+	PrintRanking();
+	StoreRanking("ranking.txt");
+	
+	return 0;
+}
